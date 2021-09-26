7bc3e5f400ee6fb3cd95233efdf664f515c7b94d
BoungChan Song <thdqudcks2@gmail.com>
2021-09-26 22:56:47 +0900

    토트넘 축구 볼거임

--- src/search.js
+++ src/search.js
@@ -10,7 +10,7 @@ const Search = ({location}) => {
   let [type,setType] = useState('');  
   let [city,setCity] = useState('');
   let [district,setDistrict] = useState(''); 
-  let [search_data,setSearchData] = useState('');
+  //let [search_data,setSearchData] = useState('');
   let [search_item,setSearchItem] = useState('');
 
   let query =qs.parse(location.search, {ignoreQueryPrefix: true});
@@ -24,32 +24,35 @@ const Search = ({location}) => {
   let [pagenationCode,setPagenationCode] = useState('');
 
     
-  useEffect(()=> {
-    if(query.hasOwnProperty('search_text')){
-      setSearch_text(query.search_text);
-    }
-    if(query.hasOwnProperty('type')){
-      setType(query.type);
-    }
-    if(query.hasOwnProperty('city')){
-      setCity(query.city);
-    }
-    
-  }, [])
 
   //apu 호출부분 
   useEffect(() => { const apiCall = async () => {
-
-    let query =qs.parse(location.search, {ignoreQueryPrefix: true});
-    let _city=query.city;
+    let query =qs.parse(location.search, {ignoreQueryPrefix: true});    
     let API_url = "";
     let json_title = 0;
 
     let newCurrent = query.p ?? 1;
     let pagenation_start = start;
     let pagenation_last = last;
-    let newPageCount =pageCount;
+    let newPageCount =pageCount == 1 ? 5 : pageCount ;
     
+    let _search_text ="";
+    let _type ="";
+    let _city ="";
+
+    if(query.hasOwnProperty('search_text')){
+      _search_text = query.search_text;
+      setSearch_text(_search_text); 
+    }        
+    if(query.hasOwnProperty('type')){
+      _type = query.type;
+      setType(_type);
+    }          
+    if(query.hasOwnProperty('city')){
+      _city = query.city;
+      setCity(_city);
+    }
+      
     console.log(newCurrent,newPageCount,pagenation_start,pagenation_last);
     setCurrent(newCurrent);
     let API_info ={
@@ -78,8 +81,7 @@ const Search = ({location}) => {
         default : 
           break;           
       }      
-    }
-    console.log(API_url);
+    }  
 
     const response = await axios.get(`${API_url}`).then(function (response) {
       // response
@@ -93,50 +95,31 @@ const Search = ({location}) => {
       }else{
       }      
       let json_array = JSON.parse(json_data);
-      setSearchData(json_array);
-      
-      if ((search_data.length + 1) % postedItemCount > 0){
-        pagenation_last = parseInt((search_data.length + 1) / postedItemCount + 1);
-        setLast(pagenation_last);
-      }else{
-        pagenation_last =parseInt((search_data.length + 1) / postedItemCount);
-        setLast(pagenation_last);
-      }
-                     
-      if(newCurrent > newPageCount){
-        if(newPageCount+5 <= last){
-          newPageCount +=5;
-        }else{                       
-          newPageCount = pagenation_last;
-        }
-        pagenation_start += 5     
-        
-      }else if(newCurrent < start){                                  
-          pagenation_start -= 5
-          newPageCount = pagenation_start * 5;                                    
-      }
-      setPageCount(newPageCount);
-      setStart(pagenation_start);
 
-      makePagenation(newCurrent,newPageCount,pagenation_start,pagenation_last);
+      //console.log(json_array);
 
+
+      var searchedData = [];
       var result = [];
       
       let slice_start = (newCurrent-1)*16;
       let slice_end =  newCurrent*16;
 
-
+      //console.log(_search_text);
       if(json_title === 0){
-        result = json_array.slice(slice_start, slice_end).map(row => 
+        searchedData = json_array.filter(row => row.INST_NM.search(_search_text) !== -1 );      
+        result = searchedData.slice(slice_start, slice_end).map(row =>                 
           <div className="item">
             <div className="title">{row.INST_NM}</div>
             <div className="address">{row.REFINE_ROADNM_ADDR}</div>
             <div className="type">{row.DIV_NM}</div>
             <div className="tag">{row.GRAD}</div>
           </div>
-        );          
+        ); 
       }else{
-        result = json_array.slice(slice_start, slice_end).map(row => 
+        searchedData = json_array.filter(row => row.FACLT_NM.search(_search_text) !== -1 );      
+        
+        result = searchedData.slice(slice_start, slice_end).map(row =>
           <div className="item">
             <div className="title">{row.FACLT_NM}</div>
             <div className="address">{row.REFINE_ROADNM_ADDR}</div>
@@ -144,11 +127,51 @@ const Search = ({location}) => {
             <div className="tag">{row.FACLT_INSTL_DETAIL_DE}</div>
           </div>
         );                      
-      }
-     
+      }    
+      
       setSearchItem(result);
-      console.log(json_array);
-      console.log(result);
+      //console.log(searchedData.length);
+      console.log(searchedData.length);
+      console.log(postedItemCount);
+      // 16개 이하일대 
+      if(searchedData.length < postedItemCount){
+        pagenation_last = 1;
+        newPageCount =1;
+        setLast(pagenation_last);
+      }else{
+        if ((searchedData.length) % postedItemCount > 0){
+          pagenation_last = parseInt((searchedData.length) / postedItemCount + 1);
+          setLast(pagenation_last);
+        }else{
+          pagenation_last =parseInt((searchedData.length) / postedItemCount);
+          setLast(pagenation_last);
+        }        
+      }
+      setPageCount(newPageCount);
+
+      //console.log(newPageCount);      
+      
+      console.log(newCurrent,newPageCount,pagenation_start,pagenation_last);
+      if(newCurrent > newPageCount){
+        if(newPageCount+5 <= last){
+          newPageCount +=5;
+        }else{                       
+          newPageCount = pagenation_last;
+        }
+        pagenation_start += 5;     
+        
+      }else if(newCurrent < start){                                  
+          pagenation_start -= 5;
+          newPageCount = pagenation_start * 5;                                    
+      }
+      setPageCount(newPageCount);
+
+      setStart(pagenation_start);
+
+      makePagenation(newCurrent,newPageCount,pagenation_start,pagenation_last);
+     
+      
+      //console.log(result);
     }).catch(function (error) {
         // 오류발생시 실행
     }).then(function() {
