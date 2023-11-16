53a46d8c1139eb671a11eb552e91636d3e0ea6e0
BoungChan Song <thdqudcks2@gmail.com>
2023-11-16 21:09:32 +0900

    react-query-devtools 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import GlobalStyles from '@styles/globalStyle'
 import Layout from '@components/layoout'
 import Home from './routes/home'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import Test from '@routes/test'
+import Search from '@routes/search'
 
 const router = createBrowserRouter([
   {
@@ -12,16 +16,27 @@ const router = createBrowserRouter([
         path: '/',
         element: <Home />,
       },
+      {
+        path: 'search',
+        element: <Search />,
+      },
+      {
+        path: 'test',
+        element: <Test />,
+      },
     ],
   },
 ])
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <GlobalStyles />
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   )
 }
 
