f1af812adac46936e15acabb1b88645bfbaebf5f
BoungChan Song <thdqudcks2@gmail.com>
2023-12-06 15:32:34 +0900

    검색 페이지 주요 변경사항 적용 및 보완

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import GlobalStyles from '@styles/globalStyle'
-import Layout from '@components/layoout'
+import Layout from '@components/layout'
+import SearchLayout from '@components/layout/searchLayout'
 import Home from './routes/home'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Test from '@routes/test'
 import Search from '@routes/search'
+import { SearchOptionProvider } from '@context/searchOptionContext'
 
 const router = createBrowserRouter([
   {
@@ -13,16 +15,22 @@ const router = createBrowserRouter([
     element: <Layout></Layout>,
     children: [
       {
-        path: '/',
+        index: true,
         element: <Home />,
-      },
-      {
-        path: 'search',
-        element: <Search />,
       },
       {
         path: 'test',
         element: <Test />,
+      },
+    ],
+  },
+  {
+    path: '/search',
+    element: <SearchLayout></SearchLayout>,
+    children: [
+      {
+        index: true,
+        element: <Search />,
       },
     ],
   },
@@ -33,9 +41,11 @@ const queryClient = new QueryClient()
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
-      <GlobalStyles />
-      <RouterProvider router={router} />
+      <SearchOptionProvider>
+        <ReactQueryDevtools />
+        <GlobalStyles />
+        <RouterProvider router={router}></RouterProvider>
+      </SearchOptionProvider>
     </QueryClientProvider>
   )
 }
--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useContext } from 'react'
 import { getHospitalAPI, getSanatoriumAPI, getWelfareServiceAPI } from '@api/openAPI'
 import { useQuery } from '@tanstack/react-query'
 import styled from 'styled-components'
@@ -5,6 +6,13 @@ import SearchedContents from '@components/searchedContents'
 import { FacilityCategory } from '@assets/staticData/facilityType'
 import { APIResponse } from '@api/type'
 import { HospitalDetailData, SanatoriumDetailData, WelfareServiceDetailData } from 'types/apiData'
+import MainSearchForm from '@components/mainSearchForm'
+import { useLocation } from 'react-router-dom'
+import SearchOptionContext from '@context/searchOptionContext'
+
+const Containor = styled.div`
+  width: 100%;
+`
 
 const SearchContainor = styled.div`
   width: 1024px;
@@ -37,9 +45,17 @@ const SearchContainor = styled.div`
 const ContentBox = styled.div``
 
 const Search = () => {
+  // const { states } = useContext(SearchOptionContext)
+  // const { searchText, facCtg, city, province, detailCtg, profit, grade, p } = states
   /** 화면에 노출 되는 콘텐츠 갯수 */
   const SHOW_ITEMS_COUNT = 16
-  let getParameter = (key: string) => new URLSearchParams(location.search).get(key)
+
+  const location = useLocation()
+
+  let getParameter = useCallback(
+    (key: string) => new URLSearchParams(location.search).get(key),
+    [location.search],
+  )
 
   const searchText = getParameter('searchText') ?? '' // 검색 내용
   // const province = getParameter('province') ?? '전체' // 행정구역 도
@@ -52,63 +68,69 @@ const Search = () => {
   const grade = getParameter('grade') ?? '전체' // 요양 병원 등급
 
   // 요양시설 + 재가노인복지시설 에 사용되는 검색 옵션
-  const detailCategory = getParameter('detailCategory') ?? '전체' // 상세 시설 분류
+  const detailCategory = getParameter('detailCtg') ?? '전체' // 상세 시설 분류
   const profit = getParameter('profit') ?? '전체' // 영리
 
-  const fetchAPI = (
-    page: number,
-  ): Promise<APIResponse<HospitalDetailData | SanatoriumDetailData | WelfareServiceDetailData>> => {
-    // 재가 노인 복지시설 + 요양시설 API param obj
-    const paramObj = {
-      SIGName: city,
-      searchText,
-      page,
-      notiCount: SHOW_ITEMS_COUNT,
-      detailCategory,
-      profit,
-    }
+  const fetchAPI = useCallback(
+    (
+      page: number,
+    ): Promise<
+      APIResponse<HospitalDetailData | SanatoriumDetailData | WelfareServiceDetailData>
+    > => {
+      // 재가 노인 복지시설 + 요양시설 API param obj
+      const paramObj = {
+        SIGName: city,
+        searchText,
+        page,
+        notiCount: SHOW_ITEMS_COUNT,
+        detailCategory,
+        profit,
+      }
 
-    // 병원용 API param obj
-    const hosParamObj = {
-      SIGName: city,
-      searchText,
-      page,
-      notiCount: SHOW_ITEMS_COUNT,
-      grade,
-    }
+      // 병원용 API param obj
+      const hosParamObj = {
+        SIGName: city,
+        searchText,
+        page,
+        notiCount: SHOW_ITEMS_COUNT,
+        grade,
+      }
 
-    switch (facCtg) {
-      case '요양병원':
-        return getHospitalAPI(hosParamObj)
-      case '요양시설':
-        return getSanatoriumAPI(paramObj)
-      case '재가노인복지시설':
-        return getWelfareServiceAPI(paramObj)
-      default:
-        throw Error
-    }
-  }
+      console.log('비동기 요청함수 들어옴')
+
+      switch (facCtg) {
+        case '요양병원':
+          return getHospitalAPI(hosParamObj)
+        case '요양시설':
+          return getSanatoriumAPI(paramObj)
+        case '재가노인복지시설':
+          return getWelfareServiceAPI(paramObj)
+        default:
+          throw Error
+      }
+    },
+    [facCtg, city, searchText, page, SHOW_ITEMS_COUNT, detailCategory, profit, grade],
+  )
 
   const { data, isLoading, error } = useQuery({
-    queryKey: ['searchedData'],
+    queryKey: ['searchedData', location.search, page],
     staleTime: 60 * 1000,
     queryFn: () => fetchAPI(page),
   })
 
   return (
-    <SearchContainor>
-      <ContentBox>
-        {data && (
-          <SearchedContents
-            data={data}
-            showItemsCount={SHOW_ITEMS_COUNT}
-            queryData={{ facCtg, city, searchText, detailCategory, profit, grade, page }}
-          />
-        )}
-        {isLoading && <>loding 중</>}
-        {error && <>error 발생</>}
-      </ContentBox>
-    </SearchContainor>
+    <Containor>
+      <MainSearchForm />
+      <SearchContainor>
+        <ContentBox>
+          {data && (
+            <SearchedContents facCtg={facCtg} data={data} showItemsCount={SHOW_ITEMS_COUNT} />
+          )}
+          {isLoading && <>loding 중</>}
+          {error && <>error 발생</>}
+        </ContentBox>
+      </SearchContainor>
+    </Containor>
   )
 }
 
