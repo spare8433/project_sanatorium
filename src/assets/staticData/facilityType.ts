959ab75100ae8aeda01e8eaba55367c020142357
BoungChan Song <thdqudcks2@gmail.com>
2023-12-13 03:29:00 +0900

    시설 관련 타입 및 데이터 업데이트

--- src/assets/staticData/facilityType.ts
+++ src/assets/staticData/facilityType.ts
@@ -6,7 +6,7 @@ export type FacilityType = '요양병원' | '요양시설' | '재가노인복지
 /**
  * 요양시설 카테고리 (type)
  */
-export type SanatoriumFacCtgType =
+export type WelfareServiceFacCtgType =
   | '방문요양'
   | '방문목욕'
   | '주야간보호'
@@ -18,12 +18,17 @@ export type SanatoriumFacCtgType =
 /**
  * 재가노인복지시설 카테고리 (type)
  */
-export type WelfareServiceFacCtgType = '노인요양공동생활가정' | '노인요양시설'
+export type SanatoriumFacCtgType = '노인요양공동생활가정' | '노인요양시설'
 
 /**
  * 요양병원 등급 종류 (type)
  */
 export type HosGradeListType = '1등급' | '2등급' | '3등급' | '4등급' | '5등급' | '등급제외'
+
+export type DetailCtgType = WelfareServiceFacCtgType | SanatoriumFacCtgType
+export type ProfitType = '영리' | '비영리'
+
+export const ProfitCategory = ['영리', '비영리']
 
 /**
  * 요양 관련 시설 카테고리
