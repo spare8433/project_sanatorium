1494028b995f9d737291b2c754f86dece45f14d3
Byeong <byeongchan8433@gmail.com>
2024-02-01 20:09:48 +0900

    CD TEST

--- src/routes/testPage.tsx
+++ src/routes/testPage.tsx
@@ -1,6 +1,11 @@
 const TestPage = () => {
   let test: string | null = ''
-  return <div>{test}</div>
+  return (
+    <div>
+      <h1>CD test</h1>
+      {test}
+    </div>
+  )
 }
 
 export default TestPage
